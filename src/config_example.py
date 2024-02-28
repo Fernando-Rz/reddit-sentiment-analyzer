c701cb1ebda26e1a3e393333b31deefc5de07fd7
Fernando Rodriguez <fernando.rodriguezb2@gmail.com>
2024-02-27 20:13:43 -0500

    init python files

---
+++ src/config_example.py
@@ -0,0 +1,7 @@
+# Configuration needed to access the Reddit API, substitute your credentials here
+# Rename this file reddit_config.py
+client_id = "CLIENT_ID"
+client_secret = "CLIENT_SECRET"
+password = "PASSWORD"
+user_agent = "USER_AGENT"
+username = "USERNAME"
---
+++ src/reddit_sentiment_analyzer.py
@@ -0,0 +1,7 @@
+import praw
+import pandas as pd
+from transformers import pipeline
+
+# TODO: Retrieve reddit post
+# TODO: Analyze the comments and the post
+# TODO: Format results
